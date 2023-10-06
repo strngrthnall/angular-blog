2e6b915c9119c78e59e3d03b1db571673ea65b84
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-10-06 18:23:40 -0300

    propriedades dinamicas dos componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -9,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  pictureCover: string = ''
+  @Input()
+  cardTitle: string = ''
+  @Input()
+  cardDescription: string = ''
+  @Input()
+  publishDate: string = ''
 
   constructor() { }
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  pictureCover: string = ''
+  @Input()
+  cardTitle: string = ''
+  @Input()
+  publishDate :string = ''
 
   constructor() { }
 
